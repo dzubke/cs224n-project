a44ba02c39c1915e519122fe83b71797364afddd
Dustin Zubke <dzubke@outlook.com>
2023-02-22 22:49:26 -0500

    checkpoint, code doesnt run yet

---
+++ src/load_data.py
@@ -0,0 +1,17 @@
+# standard libs
+import json
+from pathlib import Path
+
+
+def load_task_data(data_path):
+    data_path = Path(data_path)
+    if data_path.is_dir():
+        data = {}
+        for json_path in data_path.rglob('*.json'):
+            raise NotImplementedError
+    else:
+        if data_path.suffix == '.json':
+            with open(data_path, 'r') as fid:
+                data = json.load(fid)
+    
+    return data
--- src/semantic_similarity/semantic-sim.py
+++ src/semantic_similarity/semantic-sim.py
@@ -1,33 +1,45 @@
+# standard libs
+# installed libs
+from sentence_transformers import SentenceTransformer
+import yaml
+# project libs
+from src.load_data import load_task_data
 
 
+def main(config_path):
 
-def calc_semantic_sim_score(config):
-
-    cfg = config
+    cfg = load_config(config_path)
 
     model = load_semantic_sim_model()
 
-    data = load_data(cfg['data_path'])
-
+    data = load_task_data(cfg['data_path'])
 
     if cfg.get('load_embeddings', None):
         embeddings = load_embeddings(cfg['embedding_path'])
     else: 
         embeddings = calc_embeddings(model, data)
-        save_embeddings(embeddings, cfg['embedding_path'])
+        print(embeddings)
+        #save_embeddings(embeddings, cfg['embedding_path'])
 
 
     if cfg['embedding_aggregation'] == 'mean':
         embeddings = average_embeddings_by_category(embeddings)
 
-    cos_similarities = calc_cosine_similarity(embeddings)
+    cos_sims = calc_cosine_similarity(embeddings)
 
+
+def load_config(config_path: str)-> dict:
+    with open(config_path, 'r') as fid:
+        return yaml.load(fid, Loader=yaml.CLoader)
+    
 def load_semantic_sim_model():
-    pass
-
+    return SentenceTransformer('all-MiniLM-L6-v2')
 
 def calc_embeddings(model, data):
-    pass
+    assert isinstance(data, list)
+    assert isinstance(data[0], str)
+
+    return model.encode(data)
 
 def save_embeddings(embeddings, save_path):
     pass
@@ -40,3 +52,8 @@ def average_embeddings_by_category(embeddings):
 
 def calc_cosine_similarity(embeddings):
     pass
+
+
+if __name__ == "__main__":
+    config_path = "./semantic-sim-config.yml"
+    main(config_path)
