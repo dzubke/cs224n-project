0c72d1951f8fdf2987989176458386e324a72e62
Dustin Zubke <dzubke@outlook.com>
2023-02-25 09:46:14 -0500

    finished version of task embeddings

--- src/semantic_similarity/semantic-sim.py
+++ src/semantic_similarity/semantic-sim.py
@@ -1,14 +1,14 @@
 # standard libs
 import argparse
-from typing import List
+import json
+from typing import Dict, List
 
 # installed libs
 from sentence_transformers import SentenceTransformer
 import yaml
 
 # project libs
-from src.load_data import load_task_data
-
+from src.data_utils.load_data import load_semantic_sim_data
 
 def main(config_path):
 
@@ -19,16 +19,16 @@ def main(config_path):
     data = load_semantic_sim_data(cfg['data_path'])
 
     if cfg.get("load_embeddings", None):
-        embeddings = load_embeddings(cfg["embedding_path"])
+        embeddings_dict = load_embeddings(cfg["embedding_path"])
     else:
-        embeddings = calc_embeddings(model, data)
-        print(embeddings)
-        # save_embeddings(embeddings, cfg['embedding_path'])
+        embeddings_dict = calc_embeddings(model, data)
+        print(list(embeddings_dict.items())[:3])
+        save_embeddings(embeddings_dict, cfg['embedding_path'])
 
     if cfg["embedding_aggregation"] == "mean":
-        embeddings = average_embeddings_by_category(embeddings)
+        embeddings_dict = average_embeddings_by_category(embeddings_dict)
 
-    cos_sims = calc_cosine_similarity(embeddings)
+    cos_sims = calc_cosine_similarity(embeddings_dict)
 
 
 def load_config(config_path: str) -> dict:
@@ -40,27 +40,21 @@ def load_semantic_sim_model():
     return SentenceTransformer("all-MiniLM-L6-v2")
 
 
-def load_semantic_sim_data(data_path: str) -> List[str]:
-    json_data = load_task_data(data_path)
-    output = []
-    for task, contents in json_data.items():
-        output.append(contents["Definition"][0])
-    return output
-
-
-def calc_embeddings(model, data):
-    assert isinstance(data, list)
-    assert isinstance(data[0], str)
-
-    return model.encode(data)
+def calc_embeddings(model, data: Dict[str, str]) -> Dict[str, List[float]]:
+    embeddings = model.encode(list(data.values()))
+    return {
+        id: embed.tolist() for id, embed in zip(data.keys(), embeddings)
+    }
 
 
 def save_embeddings(embeddings, save_path):
-    pass
+    with open(save_path, 'w') as fid:
+        json.dump(embeddings, fid)
 
 
-def load_embeddings(embedding_path: str):
-    pass
+def load_embeddings(load_path: str):
+    with open(load_path, 'r') as fid:
+        return json.load(fid)
 
 
 def average_embeddings_by_category(embeddings):
